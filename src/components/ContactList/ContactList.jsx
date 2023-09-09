58a393b527d3cb6c3c4c1f513a956e10be0fb59d
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-09 13:18:03 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,11 @@
-import {Contact} from '../Contact/Contact'
+import { Contact } from '../Contact/Contact';
 
 export function ContactList({ contacts }) {
   return (
     <ul>
       {contacts.map(item => (
         <li key={item.id}>
-          <p>
           <Contact item={item} />
-          </p>
         </li>
       ))}
     </ul>
