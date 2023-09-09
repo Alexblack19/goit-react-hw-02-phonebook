a315e891d4609c0179308f75e76e8b572d5db463
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-09 13:14:43 +0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 export const Contact = ({ item }) => {
   return (
     <p>
-      {item.name}:<span>{item.number}</span>
+      {/* {item.name}:<span>{item.number}</span> */}
+      {`${item.name}:`}
+      <span>{item.number}</span>
     </p>
   );
 };
 
 Contact.propTypes = {
-  item: PropTypes.shape.isRequired,
+  item: PropTypes.shape().isRequired,
 };
